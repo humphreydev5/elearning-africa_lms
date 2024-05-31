bc4c55515fe34e5058310d9ee983fbf37eac2044
Humphrey Ikhalea <humphreyikhalea@gmail.com>
2024-05-31 08:43:00 +0100

    final testing and debugging stage

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,103 +1,17 @@
-// import Mux from "@mux/mux-node";
-// import { auth } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
-
-// import { db } from "@/lib/db";
-
-
-// const { Video } = new Mux(
-//   process.env.MUX_TOKEN_ID,
-//   process.env.MUX_TOKEN_SECRET,
-// );
-
-// export async function DELETE(
-//   req: Request,
-//   { params }: { params: { courseId: string } }
-// ) {
-//   try {
-//     const { userId } = auth();
-
-//     if (!userId) {
-//       return new NextResponse("Unauthorized", { status: 401 });
-//     }
-
-//     const course = await db.course.findUnique({
-//       where: {
-//         id: params.courseId,
-//         userId: userId,
-//       },
-//       include: {
-//         chapters: {
-//           include: {
-//             muxData: true,
-//           }
-//         }
-//       }
-//     });
-
-//     if (!course) {
-//       return new NextResponse("Not found", { status: 404 });
-//     }
-
-//     for (const chapter of course.chapters) {
-//       if (chapter.muxData?.assetId) {
-//         await Video.Assets.del(chapter.muxData.assetId);
-//       }
-//     }
-
-//     const deletedCourse = await db.course.delete({
-//       where: {
-//         id: params.courseId,
-//       },
-//     });
-
-//     return NextResponse.json(deletedCourse);
-//   } catch (error) {
-//     console.log("[COURSE_ID_DELETE]", error);
-//     return new NextResponse("Internal Error", { status: 500 });
-//   }
-// }
-
-// export async function PATCH(
-//   req: Request,
-//   { params }: { params: { courseId: string } }
-// ) {
-//   try {
-//     const { userId } = auth();
-//     const { courseId } = params;
-//     const values = await req.json();
-
-//     if (!userId) {
-//       return new NextResponse("Unauthorized", { status: 401 });
-//     }
-
-//     const course = await db.course.update({
-//       where: {
-//         id: courseId,
-//         userId
-//       },
-//       data: {
-//         ...values,
-//       }
-//     });
-
-//     return NextResponse.json(course);
-//   } catch (error) {
-//     console.log("[COURSE_ID]", error);
-//     return new NextResponse("Internal Error", { status: 500 });
-//   }
-// }
-
-
 import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
-const { Video } = new Mux(
-  process.env.MUX_TOKEN_ID!,
-  process.env.MUX_TOKEN_SECRET!,
-);
+// const { Video }:any = new Mux(
+//   process.env.MUX_TOKEN_ID!,
+//   process.env.MUX_TOKEN_SECRET!,
+// );
+
+const mux = new Mux({
+  tokenId: process.env['MUX_TOKEN_ID'],
+  tokenSecret: process.env['MUX_TOKEN_SECRET'],
+});
 
 export async function DELETE(
   req: Request,
@@ -107,7 +21,7 @@ export async function DELETE(
     const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
     }
 
     const course = await db.course.findUnique({
@@ -119,18 +33,18 @@ export async function DELETE(
         chapters: {
           include: {
             muxData: true,
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
     if (!course) {
-      return new NextResponse("Not Found", { status: 404 });
+      return new NextResponse(JSON.stringify({ message: "Not Found" }), { status: 404 });
     }
 
     for (const chapter of course.chapters) {
       if (chapter.muxData?.assetId) {
-        await Video.Assets.del(chapter.muxData.assetId);
+        await mux.video.assets.delete(chapter.muxData.assetId);
       }
     }
 
@@ -143,7 +57,7 @@ export async function DELETE(
     return NextResponse.json(deletedCourse);
   } catch (error) {
     console.error("[COURSE_DELETE_ERROR]", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
   }
 }
 
@@ -156,7 +70,7 @@ export async function PATCH(
     const values = await req.json();
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
     }
 
     const course = await db.course.update({
@@ -166,12 +80,12 @@ export async function PATCH(
       },
       data: {
         ...values,
-      }
+      },
     });
 
     return NextResponse.json(course);
   } catch (error) {
     console.error("[COURSE_UPDATE_ERROR]", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
   }
 }
